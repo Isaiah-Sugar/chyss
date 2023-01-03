a924e2ca24271e0921b4826015abf37c26132a85
Isaiah Sugar <readingmaniac2004@gmail.com>
2023-01-03 11:22:49 -0500

    camera movement, and sorcerer model

--- chyss-godot-proj/pieces-scenes/Changeling.gd
+++ chyss-godot-proj/pieces-scenes/Changeling.gd
@@ -8,7 +8,7 @@ var newFrog = preload("res://pieces-scenes/frog.tscn")
 var newBishop = preload("res://pieces-scenes/bishop.tscn")
 #var newPawn = preload("res://Pawn.tscn")
 #var newWheel = preload("res://Wheel.tscn")
-var newSorcerer = preload("res://pieces-scenes/Sorcerer.tscn")
+var newSorcerer = preload("res://pieces-scenes/sorcerer.tscn")
 var newRock = preload("res://pieces-scenes/rock.tscn")
 
 #list of the pieces to pick a random one from
@@ -38,7 +38,7 @@ func newChildPiece():
 	
 	#get random piece
 	var piece = piecesArray[randi() % piecesArray.size()].instance()
-	piece.visible = false
+#	piece.visible = false
 	
 	#set properties and add it
 	piece.boardPosition = self.boardPosition

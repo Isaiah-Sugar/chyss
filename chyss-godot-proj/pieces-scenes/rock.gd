ef4e049828a02bddef3a5c189ad93cd5a38936f1
Isaiah Sugar <readingmaniac2004@gmail.com>
2023-01-04 11:56:26 -0500

    pawn model, changeling team fix, made rock compatible

--- chyss-godot-proj/pieces-scenes/rock.gd
+++ chyss-godot-proj/pieces-scenes/rock.gd
@@ -2,12 +2,10 @@ extends "res://piece-outline.gd"
 
 var moveVector = Vector2(0, 0)
 
+func individual_ready():
+	whiteTeamMaterial = load("res://rock.material")
+	blackTeamMaterial = load("res://rock.material")
+
 func find_moves():
 	var validMoves = [moveVector]
 	return validMoves
-
-
-#i am isaiah adding these functions:
-func set_team(newTeam):
-	team = newTeam #value gets set
-	#do not change material based on team

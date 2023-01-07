1b15fbe25d6cf7accd584add8bd1b9e383b5ad9a
Abby Smith <asillylittlecreature@gmail.com>
2023-01-07 12:44:50 -0500

    fixed it

--- chyss-godot-proj/rayCast.gd
+++ chyss-godot-proj/rayCast.gd
@@ -3,18 +3,7 @@ extends RayCast
 onready var camera = get_parent()
 const ray_length = 1000
 
-var hoveredObject
-var mousePosition
-
-func _input(event):
-	if event is InputEventMouseMotion:
-	#set raycast destination
-		cast_ray()
-		mousePosition = event.position
-		#print(mousePosition)
-		#hoveredObject = cast_ray()
-
 func cast_ray():
 	cast_to = global_translation + camera.project_local_ray_normal(get_viewport().get_mouse_position()) * ray_length
-	print(get_collider())
-	return get_collider() #update hovered object
+	force_raycast_update()
+	return get_collider() #return hovered object

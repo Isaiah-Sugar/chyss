d6f9accb2257a0c238c0aaba3e2dfacbfe9e4abf
Abby Smith <asillylittlecreature@gmail.com>
2023-01-14 14:47:13 -0500

    Made it work

--- chyss-godot-proj/scripts/dialogue.gd
+++ chyss-godot-proj/scripts/dialogue.gd
@@ -3,18 +3,40 @@ extends Spatial
 var dialogue = preload("res://dialogue/test dialogue.tres")
 var frogFlag = false
 
+#to add a dialogue case make a bool function for its execution, the name of its dialogue node, if you want it to repeat, set alreadyHappened to null 
 var caseArray = [
-					{function = Expression.new().parse("opening_dialogue(turnNumber)", ["turnNumber"]), diaolgueNode = "opening_dialogue", alreadyHappened = false},
-					{function = Expression.new().parse("frog_reveal(move)", ["move"]), dialogueNode = "frog_reveal", alreadyHappened = false}
+					{function = "opening_dialogue(move, turnNumber)", dialogueNode = "opening_dialogue", alreadyHappened = false},
+					{function = "frog_reveal(move, turnNumber)", dialogueNode = "frog_reveal", alreadyHappened = false}
 																																]
 
 func determine_dialogue(move, turnNumber):
 	for case in caseArray:
-		print (case.function)
-		if case.function.execute() && !case.alreadyHappened:
+		if case.alreadyHappened:
+			continue
+		
+		if evaluate(case.function, ["move", "turnNumber"], [move, turnNumber]):
 			DialogueManager.show_example_dialogue_balloon(case.dialogueNode, dialogue)
-			return
+			if case.alreadyHappened && case.alredyHappened == false:
+				case.alreadyHappened = true
 
-func frog_reveal(move):
+func evaluate(command, variable_names = [], variable_values = []):
+	var expression = Expression.new()
+	var error = expression.parse(command, variable_names)
+	if error != OK:
+		push_error(expression.get_error_text())
+		return
+	
+	var result = expression.execute(variable_values, self)
+	
+	if not expression.has_execute_failed():
+		return result
+
+func opening_dialogue(_move, turnNumber : int) -> bool:
+	if turnNumber == 0:
+		return true
+	return false
+
+func frog_reveal(move, _turnNumber : int) -> bool:
 	if move && move.capture && move.capture.type == "Hat":
 		return true
+	return false

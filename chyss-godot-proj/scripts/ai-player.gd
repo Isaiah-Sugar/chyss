211dea3c6fcbc797289dd933a3e41e3304800573
Abby Smith <asillylittlecreature@gmail.com>
2023-02-08 13:00:00 -0500

    Ai has a 10% chance to jail a piece on their turn, jail info is passed to dialogue

--- chyss-godot-proj/scripts/ai-player.gd
+++ chyss-godot-proj/scripts/ai-player.gd
@@ -4,6 +4,8 @@ extends Player
 #On its turn it finds all its moves, scores them, then picks a move and makes it
 
 onready var thinkTimer = get_node("ThinkTimer")
+var jailedPiece = null
+const jailPosition = Vector2(-1.8, 3.5)
 
 #function to play the ai's turn
 func play_turn():
@@ -11,15 +13,24 @@ func play_turn():
 	thinkTimer.start()
 	yield(thinkTimer, "timeout")
 	
-	#find all your moves
-	var movesArray = find_moves()
-	#score the moves
-	var dangerArray = find_danger()
-	for move in movesArray:
-		score_move(move, dangerArray)
-	#pick a move and make it
-	var move = pick_move(movesArray)
-	make_move(move)
+	#90% chance to make a normal move
+	if randi() % 10 > 0:
+		#find all your moves
+		var movesArray = find_moves()
+		#score the moves
+		var dangerArray = find_danger()
+		for move in movesArray:
+			score_move(move, dangerArray)
+		#pick a move and make it
+		var move = pick_move(movesArray)
+		make_move(move)
+	#10% chance to jail a piece
+	else:
+		#pick a random piece
+		var pieces = pieceParent.get_children()
+		var jailPiece = pieces[randi() % pieces.size()]
+		#send it to jail
+		jail_piece(jailPiece)
 
 #fancy custom sorter
 class ScoreSorter:
@@ -41,3 +52,18 @@ func pick_move(movesArray):
 	#pick a random remaining move
 	var move = movesArray[randi() % movesArray.size()]
 	return move
+
+func jail_piece(piece):
+	#unjail current piece
+	if jailedPiece:
+		jailedPiece.boardPosition = piece.boardPosition
+		jailedPiece.jailed = false
+	#jail new piece
+	jailedPiece = piece
+	piece.jailed = true
+	piece.boardPosition = jailPosition
+	if piece.type == "Wheel":
+		piece.velocity = Vector2(0, 0)
+	#append dialogue and emit signal
+	Dialogue.append_queue(["jailedPiece"], [piece])
+	board.call_deferred("emit_signal", "move_made")
